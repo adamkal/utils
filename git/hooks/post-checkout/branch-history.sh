5ee9866afef13145c2511a053f96049a94d7094c
Adam Kalinski <adamkalinski@gmail.com>
2016-10-12 17:32:47 +0200

    Improve code?

--- git/hooks/post-checkout/branch-history.sh
+++ git/hooks/post-checkout/branch-history.sh
@@ -3,9 +3,11 @@
 # This hook will log all branches you've been working on
 # recently and save it to ~/.git-branch-history
 
+
 HIST_FILE=$HOME/.git-branch-history
 TMP_FILE=$(mktemp)
-echo "$(git rev-parse --abbrev-ref HEAD)" >> $HIST_FILE
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+echo "$BRANCH_NAME" >> $HIST_FILE
 cat $HIST_FILE | uniq > $TMP_FILE
 cp $TMP_FILE $HIST_FILE 
 
